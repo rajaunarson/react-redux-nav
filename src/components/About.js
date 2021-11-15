9d9d5ada906625a9ff25a7440c56c0ddab6b773f
rajaunarson <rajaunarson94@gmail.com>
2021-11-15 21:19:01 +0530

    Spread

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,39 @@
 import React from 'react';
 
-export default function About() {
-  return (
-    <div>
-      <h1> This is About Page</h1>
-    </div>
-  );
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: [
+        { id: 1, someattr: 'a string', anotherattr: '' },
+        { id: 2, someattr: 'another string', anotherattr: '' },
+        { id: 3, someattr: '3rd a string', anotherattr: '' },
+      ],
+
+      // user: { id: 1, someattr: "a string", anotherattr: "" }
+    };
+  }
+
+  addClick = () => {
+    this.setState({
+      user: [
+        ...this.state.user,
+        { id: 4, someattr: '4rd a string', anotherattr: '' },
+      ],
+    });
+  };
+
+  render() {
+    const userdet = this.state.user;
+    return (
+      <div>
+        {userdet.map(function (d, idx) {
+          return <li key={idx}>{d.someattr}</li>;
+        })}
+        <button onClick={this.addClick}> Add</button>
+      </div>
+    );
+  }
 }
+
+export default About;
